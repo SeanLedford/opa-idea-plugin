4d6b02d43e9b0140cd920fb5c7d15bbac8b5163a
seanledford <s_ledford@apple.com>
2026-03-15 20:51:29 -0500

    Add Semantic Token LSP request support and test coverage
    Signed-off-by: seanledford <s_ledford@apple.com>

---
+++ src/main/kotlin/org/openpolicyagent/ideaplugin/lsp/RegalSemanticTokensColorsProvider.kt
@@ -0,0 +1,30 @@
+package org.openpolicyagent.ideaplugin.lsp
+
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.psi.PsiFile
+import com.redhat.devtools.lsp4ij.features.semanticTokens.SemanticTokensColorsProvider
+
+class RegalSemanticTokensColorsProvider : SemanticTokensColorsProvider {
+    /**
+     * Returns the [TextAttributesKey] to use for colorization for the given token type and given token modifiers and null otherwise.
+     *
+     * @param tokenType      the token type.
+     * @param tokenModifiers the token modifiers.
+     * @param file           the Psi file.
+     * @return the [TextAttributesKey] to use for colorization for the given token type and given token modifiers and null otherwise.
+     */
+    override fun getTextAttributesKey(
+        tokenType: String,
+        tokenModifiers: List<String>,
+        file: PsiFile
+    ): TextAttributesKey? = when (tokenType) {
+        "namespace" -> DefaultLanguageHighlighterColors.STRING
+        "variable" -> when {
+            tokenModifiers.contains("declaration") -> DefaultLanguageHighlighterColors.CONSTANT
+            tokenModifiers.contains("reference")   -> DefaultLanguageHighlighterColors.CONSTANT
+            else -> null
+        }
+        else -> null
+    }
+}
